3f1f4dad3e7b529d79755ec6bfb6becae6c9906d
Cillian Long <longci@tcd.ie>
2017-06-03 13:24:30 +0100

    Remove forum files

--- forum/cache/data_ranks.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1504959381
-271
-a:1:{s:7:"special";a:2:{i:1;a:4:{s:7:"rank_id";s:1:"1";s:10:"rank_title";s:9:"Developer";s:12:"rank_special";s:1:"1";s:10:"rank_image";s:0:"";}i:2;a:4:{s:7:"rank_id";s:1:"2";s:10:"rank_title";s:15:"VATeir Director";s:12:"rank_special";s:1:"1";s:10:"rank_image";s:0:"";}}}
--- forum/cache/sql_f0bf07fa26cf046860a9b00132ec37bd.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-1487158294
-SELECT * FROM phpbb_bbcodes WHERE bbcode_id = 14
-633
-a:1:{i:0;a:10:{s:9:"bbcode_id";s:2:"14";s:10:"bbcode_tag";s:6:"center";s:15:"bbcode_helpline";s:11:"Center text";s:18:"display_on_posting";s:1:"0";s:12:"bbcode_match";s:23:"[center]{TEXT}[/center]";s:10:"bbcode_tpl";s:32:"<div align="center">{TEXT}</div>";s:16:"first_pass_match";s:31:"!\[center\](.*?)\[/center\]!ies";s:18:"first_pass_replace";s:140:"'[center:$uid]'.str_replace(array("\r\n", '\"', '\'', '(', ')'), array("\n", '"', '&#39;', '&#40;', '&#41;'), trim('${1}')).'[/center:$uid]'";s:17:"second_pass_match";s:39:"!\[center:$uid\](.*?)\[/center:$uid\]!s";s:19:"second_pass_replace";s:30:"<div align="center">${1}</div>";}}
