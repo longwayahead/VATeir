118f4c4e49fb84d4e8a44755d37995f1d4b4ec7c
longwayahead <longci@tcd.ie>
2017-07-08 17:58:32 +0100

    Background image loading via HTTPS

---
+++ classes/Twitch.php
@@ -0,0 +1,33 @@
+<?php
+class Twitch {
+
+  private function getFromTwitch() {
+    $curl = curl_init();
+    //curl_setopt($curl, CURLOPT_URL, 'https://api.twitch.tv/kraken/streams/122441256'); //Set the URL here
+    curl_setopt($curl, CURLOPT_URL, 'https://api.twitch.tv/kraken/streams/49140130'); //Set the URL here
+    //curl_setopt($curl, CURLOPT_URL, 'https://api.twitch.tv/kraken/users?login=HandIOfIBlood'); //Set the URL here
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLINFO_HEADER_OUT, true);
+
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+      'Accept: application/vnd.twitchtv.v5+json',
+      'Client-ID: '.Config::get('twitch/apitoken')
+    ));
+
+
+    $get = curl_exec($curl); //Get the data
+    curl_close($curl);
+    return $get;
+  }
+
+  public function johnStream() {
+    $details = $this->getFromTwitch();
+    if($details->stream == null) {
+      return false;
+    } else {
+      return $details;
+    }
+  }
+
+}
