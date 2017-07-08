6c853d5ceb9a58668c09a927e1c6f5429c8de2a3
longwayahead <longci@tcd.ie>
2017-07-08 17:06:02 +0100

    Improve reliability of NATs by moving them to new API

--- pilots/tracks.php
+++ pilots/tracks.php
@@ -1,22 +1,21 @@
 <?php
 $pagetitle = "North Atlantic Tracks";
 require_once('includes/header.php');
-$d = new Download;
-$tracks = $d->blackswan();
+$oad = new OAD;
+$tracks = json_decode(json_encode($oad->nats()), true);
 // echo '<pre>';
 // print_r($tracks);
 // echo '</pre>';
 ?>
-<h3 class="text-center">Oceanic Tracks</h3>
+<h3 class="text-center">Oceanic Tracks</h3><br>
 <?php
 $datetime = new DateTime();
 $datetime->modify('+1 day');
 ?>
-<div class="text-center" style="font-size:20px;">Today is day <strong><?php echo $datetime->format('z'); ?></strong> of the year.</div>
+<div class="text-center" style="font-size:20px;">Today is day <strong><?php echo $datetime->format('z'); ?></strong> of the year.</div><br>
 <div class="row">
 	<div class="col-md-8 col-md-offset-2">
 	<?php
-	//	$tracks = $d->oadAPI('tracks', ['cache' => 1])[0]->nats->routes;
 		?>
 		<div class="panel panel-info">
 			<div class="panel-heading">
@@ -27,36 +26,35 @@ $datetime->modify('+1 day');
 				<table class="table table-condensed table-responsive">
 					<tr>
 						<td colspan="2"><div class="text-center"><strong>Track</strong></div></td>
-						<td><strong>FL / Direction</strong></td>
-						<td><strong>TMI</strong></td>
-						<!-- <td><strong>Valid From</strong></td>
-						<td><strong>Valid Until</strong></td> -->
+						<td><strong>Direction</strong></td>
+						<td><strong>Valid From</strong></td>
+						<td><strong>Valid Until</strong></td>
 					</tr>
 
 					<?php
 						foreach($tracks as $track) {
 							?>
 							<tr>
-								<td><strong><?php echo $track['@attributes']['id']; ?></strong></td>
+								<td><strong><?php echo $track['name']; ?></strong></td>
 								<td>
-									<?php foreach($track['waypoints']['waypoint'] as $waypoint) {
-										echo $waypoint['@attributes']['id'] . '<br>';
+									<?php foreach($track['navdata'] as $waypoint) {
+										echo $waypoint['ident'] . '<br>';
 									}	?>
 								</td>
 								<td>
-									<?php foreach($track['levels']['level'] as $level) {
-										echo $level['@attributes']['fl'] . ' / ' . $level['@attributes']['direction'] . '<br>';
-									}	?>
+									<?php
+										echo $track['direction'] . '<br>';
+								?>
 								</td>
-								<td><?php echo $track['@attributes']['tmi']; ?></td>
-								<!-- <td><?php
-									//$from = new DateTime($track['@attributes']['validfrom']);
-									//echo $from->format('jS F \<\b\r\>\<\s\t\r\o\n\g\>g.ia\<\s\t\r\o\n\g\>'); ?>
+								<td>
+								<?php
+									$from = new DateTime($track['start']);
+									echo $from->format('jS F \<\b\r\>\<\s\t\r\o\n\g\>g.ia\<\s\t\r\o\n\g\>'); ?>
 								</td>
 								<td><?php
-									//$until = new DateTime($track['@attributes']['validuntil']);
-									//echo $until->format('jS F \<\b\r\>\<\s\t\r\o\n\g\>g.ia\<\s\t\r\o\n\g\>'); ?>
-								</td> -->
+									$until = new DateTime($track['end']);
+									echo $until->format('jS F \<\b\r\>\<\s\t\r\o\n\g\>g.ia\<\s\t\r\o\n\g\>'); ?>
+								</td>
 							</tr>
 						<?php
 						}
