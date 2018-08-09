953dc5529837e89e4bfbfe1cf1fda040db27efaa
longwayahead <longci@tcd.ie>
2018-08-09 13:52:42 +0100

    Allow uploading of text files

--- classes/Validate.php
+++ classes/Validate.php
@@ -76,7 +76,7 @@ class Validate {
 							}
 						break;
 						case 'fileType':
-							$types = ['application/zip', 'application/x-zip-compressed'];
+							$types = ['application/zip', 'application/x-zip-compressed', 'text/plain'];
 							if(!in_array($rule_value, $types)) {
 								$this->addError("File must be a ZIP.");
 							}
